51c4942e32117d0a3ae8511ff8f36ecaa381e3ca
吴俊 <8400684@qq.com>
2023-04-17 14:28:24 +0800

    ini

---
+++ Server/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Connections;
+using SuperSocket;
+using SuperSocket.Kestrel;
+using SuperSocket.ProtoBase;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var serverOptions = builder.Configuration.GetSection("ServerOptions").Get<ServerOptions>()!;
+
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    foreach (var listeners in serverOptions.Listeners)
+    {
+        options.Listen(listeners.GetListenEndPoint(), listenOptions =>
+        {
+            listenOptions.UseConnectionHandler<KestrelChannelCreator>();
+        });
+    }
+});
+
+builder.Host.AsSuperSocketHostBuilder<StringPackageInfo, CommandLinePipelineFilter>()
+            .UseKestrelChannelCreatorFactory()
+            .AsMinimalApiHostBuilder()
+            .ConfigureHostBuilder();
+
+var app = builder.Build();
+
+await app.RunAsync();
