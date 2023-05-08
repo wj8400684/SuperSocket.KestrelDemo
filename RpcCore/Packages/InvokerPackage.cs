30720958f195b4849de0be9f507eb7090361417e
吴俊 <8400684@qq.com>
2023-05-09 01:18:09 +0800

    ini

---
+++ RpcCore/Packages/InvokerPackage.cs
@@ -0,0 +1,36 @@
+﻿using MemoryPack;
+using System.Buffers;
+
+namespace RpcCore.Packages;
+
+[MemoryPackable]
+public sealed partial class InvokerPackage : RpcPackageBase
+{
+    public InvokerPackage() 
+        : base(CommandKey.Invoker)
+    {
+    }
+
+    public ReadOnlyMemory<byte> Body { get; set; }
+
+    public override int Encode(IBufferWriter<byte> bufWriter)
+    {
+        var length = base.Encode(bufWriter);
+
+        return length;
+    }
+
+    protected internal override void DecodeBody(ref SequenceReader<byte> reader, object? context)
+    {
+        base.DecodeBody(ref reader, context);
+    }
+}
+
+[MemoryPackable]
+public sealed partial class InvokerRespPackage : RpcRespPackage
+{
+    public InvokerRespPackage()
+        : base(CommandKey.InvokerAck)
+    {
+    }
+}
